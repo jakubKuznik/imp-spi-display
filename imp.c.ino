e5456f7781084a5603254c5c5019b3900491af21
jkuznik <jakub.kuznik@adaptivemobile.com>
2022-12-07 19:53:03 +0100

    DONE 2.0

--- imp.c.ino
+++ imp.c.ino
@@ -20,8 +20,8 @@ void blinkLed(uint8_t gpio, int time);
 #define DISPLAY_X_SIZE 128
 #define DISPLAY_Y_SIZE 128
 
-#define SAD 0
-#define HAPPY 1
+#define NORMAL 0
+#define NERD 1
 
 /* Put your SSID & Password */
 const char* ssid = "sietocky_lover";  // Enter SSID here
@@ -63,10 +63,10 @@ void setup(void) {
   delay(100);
 
   server.on("/", handleOnConnect);\
-  server.on("/smile", handleSmile);
-  server.on("/sad", handleSad);
+  server.on("/nerd", handleNerd);
+  server.on("/normal", handleNormal);
 
-  server.onNotFound(handle_NotFound);
+  server.onNotFound(handleNotFound);
   
   server.begin();
   Serial.println("HTTP server started");
@@ -80,22 +80,26 @@ void setup(void) {
 
 }
 
-void handle_NotFound(){
+void handleNotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
 void handleOnConnect() {
 
   Serial.println("GPIO4 Status: OFF | GPIO5 Status: OFF");
-  server.send(200, "text/html", generateHTML(1,2)); 
+  server.send(200, "text/html", generateHTML()); 
 }
 
-void handleSad() {
-  displayFace(SAD);
+void handleNormal() {
+  displayFace(NORMAL);
+  server.send(200, "text/html", generateHTML()); 
+
 }
 
-void handleSmile() {
-  displayFace(HAPPY);
+void handleNerd() {
+  displayFace(NERD);
+  server.send(200, "text/html", generateHTML()); 
+
 }
 
 /*
@@ -107,13 +111,12 @@ void handleSmile() {
 </head>
 <body>
   <h2>IMP 2022</h2>
-
-  <button type="button">Smile</button>
-  <button type="button">Sad</button>
+  <a href=\"/nerd\">NERD</a>;
+  <a href=\"/normal\">NORMAL</a>"
 </body>
 */
 
-String generateHTML(uint8_t led1stat,uint8_t led2stat){
+String generateHTML(){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<html lang=\"en\">\n";
   ptr +="<head>\n";
@@ -121,8 +124,8 @@ String generateHTML(uint8_t led1stat,uint8_t led2stat){
   ptr +="</head>\n";
   ptr +="<body>\n";
   ptr +="<h2>IMP 2022</h2>\n";
-  ptr +="<a href=\"/smile\">SMILE</a>\n";
-  ptr +="<a href=\"/sad\">SAD</a>\n";
+  ptr +="<a href=\"/nerd\">NERD</a>\n";
+  ptr +="<a href=\"/normal\">NORMAL</a>\n";
   ptr +="</body>\n";
   return ptr;
 }
@@ -136,7 +139,7 @@ void displayText(char *text, uint16_t color){
 }
 
 
-// mood can be SAD or HAPPY
+// mood can be NERD or NORMALL
 void displayFace(bool mood) {
   // Head
   tft.fillCircle(DISPLAY_X_SIZE/2,DISPLAY_Y_SIZE/2, 40, ST7735_YELLOW);
@@ -149,14 +152,13 @@ void displayFace(bool mood) {
   // mounth
   tft.drawLine(55, 75, 75, 75, ST7735_BLACK); 
 
-  // glasses 
-  tft.drawCircle(45, 50, 10, ST7735_BLACK);
-  tft.drawCircle(80, 50, 10, ST7735_BLACK);  
-  tft.drawLine(55, 50, 70, 50, ST7735_BLACK);  
-  tft.drawLine(35, 50, 10, 50, ST7735_BLACK);  
-  tft.drawLine(90, 50, 120, 50, ST7735_BLACK); 
-
-  if (mood == HAPPY){
+  if (mood == NERD){
+    // glasses 
+    tft.drawCircle(45, 50, 10, ST7735_BLACK);
+    tft.drawCircle(80, 50, 10, ST7735_BLACK);  
+    tft.drawLine(55, 50, 70, 50, ST7735_BLACK);  
+    tft.drawLine(35, 50, 10, 50, ST7735_BLACK);  
+    tft.drawLine(90, 50, 120, 50, ST7735_BLACK); 
     uint8_t y = 75;
     uint8_t x = 55;
     for (;x <= 75; x++){
